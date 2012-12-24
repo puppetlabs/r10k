d9a87745ffb3b3b279e0f13b49f334643273c98d
Adrien Thebo <git@somethingsinistral.net>
2012-12-23 22:36:00 -0800

    Move update cache option to cli/env/deploy

--- lib/r10k/cli/environment/deploy.rb
+++ lib/r10k/cli/environment/deploy.rb
@@ -13,19 +13,24 @@ module R10K::CLI::Environment::Deploy
 
       flag :r, :recurse, 'Recursively update submodules'
 
+      flag :u, :update, "Don't update module cache data"
+
       run do |opts, args, cmd|
         deployment = R10K::Deployment.instance
+        env_list   = deployment.environments
+
+        update_cache = (defined? opts[:update]) ? opts[:update] : true
 
         if opts[:environment]
-          environments = deployment.environments.select {|env| env.name == opts[:environment]}
+          environments = env_list.select {|env| env.name == opts[:environment]}
         else
-          environments = deployment.environments
+          environments = env_list
         end
 
         environments.each do |env|
-          puts "Making environment #{env.name}"
+          puts "Synchronizing environment #{env.name}"
           FileUtils.mkdir_p env.full_path
-          env.sync!
+          env.sync! :update_cache => update_cache
         end
       end
     end
