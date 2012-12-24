4a2e8a0e054e47578a6b7ea7ade34e1d46393d2a
Adrien Thebo <git@somethingsinistral.net>
2012-12-24 15:14:49 -0800

    Clean up comments for R10K::Module

--- lib/r10k/module.rb
+++ lib/r10k/module.rb
@@ -2,16 +2,31 @@ require 'r10k'
 
 class R10K::Module
 
+  # Register an inheriting  class for later generation
   def self.inherited(klass)
     @klasses ||= []
     @klasses << klass
   end
 
+  # Look up the implementing class and instantiate an object
+  #
+  # This method takes the arguments for normal object generation and checks all
+  # inheriting classes to see if they implement the behavior needed to create
+  # the requested object. It selects the first class that can implement an object
+  # with `name, args`, and generates an object of that class.
+  #
+  # @param [String] name The unique name of the module
+  # @param [String] path The root path to install the module in
+  # @param [Object] args An arbitary value or set of values that specifies the implementation
+  #
+  # @return [Object < R10K::Module] A member of the implementing subclass
   def self.new(name, path, args)
-    if implementing_klass = @klasses.find { |klass| klass.implements(name, args) }
-      implementing_klass.new(name, path, args)
+    if implementation = @klasses.find { |klass| klass.implements(name, args) }
+      obj = implementation.send(:allocate)
+      obj.send(:initialize, name, path, args)
+      obj
     else
-      raise "Module #{name} with args #{args.inspect} can't be recognized."
+      raise "Module #{name} with args #{args.inspect} doesn't have an implementation. (Are you using the right arguments?)"
     end
   end
 
