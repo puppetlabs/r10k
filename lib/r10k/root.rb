3381d929acdca587b326240131c6b40316fb5050
Adrien Thebo <git@somethingsinistral.net>
2012-12-25 00:17:48 -0800

    Convert Root#initialize signature to hash, rename var

--- lib/r10k/root.rb
+++ lib/r10k/root.rb
@@ -16,17 +16,17 @@ class R10K::Root
   #   The location of the remote git repository
   attr_reader :remote
 
-  # @!attribute [r] branch
-  #   The git branch to instantiate into the basedir
-  attr_reader :branch
+  # @!attribute [r] ref
+  #   The git ref to instantiate into the basedir
+  attr_reader :ref
 
-  def initialize(name, basedir, remote, branch)
-    @name, @basedir, @remote, @branch = name, basedir, remote, branch
+  def initialize(hash)
+    parse_initialize_hash(hash)
   end
 
   def sync!(options = {})
     synchro = R10K::Synchro::Git.new(@remote)
-    synchro.sync(full_path, @branch, options)
+    synchro.sync(full_path, @ref, options)
   end
 
   def modules
@@ -45,5 +45,41 @@ class R10K::Root
 
   def full_path
     File.expand_path(File.join @basedir, @name)
+  end
+
+  private
+
+  def parse_initialize_hash(hash)
+    if hash[:name]
+      @name = hash.delete(:name)
+    elsif hash[:ref]
+      @name = hash[:ref]
+    else
+      raise "Unable to resolve directory name from options #{hash.inspect}"
+    end
+
+    # XXX This could be metaprogrammed, but it seems like the road to madness.
+
+    if hash[:basedir]
+      @basedir = hash.delete(:basedir)
+    else
+      raise ":basedir is a required value for #{self.class}.new"
+    end
+
+    if hash[:remote]
+      @remote = hash.delete(:remote)
+    else
+      raise ":remote is a required value for #{self.class}.new"
+    end
+
+    if hash[:ref]
+      @ref = hash.delete(:ref)
+    else
+      raise ":ref is a required value for #{self.class}.new"
+    end
+
+    unless hash.empty?
+      raise "#{self.class}.new only expects keys [:name, :basedir, :remote, :ref], got #{hash.keys.inspect}"
+    end
   end
 end
