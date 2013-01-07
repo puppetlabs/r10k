db4b735e3707a8262350d4ac8ab1b15adde3c3e9
Adrien Thebo <git@somethingsinistral.net>
2013-01-07 12:25:42 -0800

    Monkey patch logging monkey patch

--- lib/r10k/cli/ext/logging.rb
+++ lib/r10k/cli/ext/logging.rb
@@ -4,5 +4,13 @@ require 'r10k/logging'
 module Cri
   class CommandDSL
     include R10K::Logging
+
+    def logger
+      unless @logger
+        @logger = Log4r::Logger.new(@command.name)
+        @logger.add R10K::Logging.outputter
+      end
+      @logger
+    end
   end
 end
