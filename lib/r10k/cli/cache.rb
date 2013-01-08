ca1fd93ac505feae0fe26c273145b9414f05c14a
Adrien Thebo <git@somethingsinistral.net>
2013-01-08 10:17:15 -0800

    Remove obsolute [] call for cli cache option

--- lib/r10k/cli/cache.rb
+++ lib/r10k/cli/cache.rb
@@ -12,7 +12,7 @@ module R10K::CLI
         summary 'Update cache for all sources'
 
         run do |opts, args, cmd|
-          sources = R10K::Deployment.instance[:sources]
+          sources = R10K::Deployment.config[:sources]
           remotes = Set.new
 
           sources.each_pair do |name, hash|
@@ -20,7 +20,7 @@ module R10K::CLI
           end
 
           remotes.each do |remote|
-            logger.info "Synchronizing #{remote}"
+            logger.info "Updating cache for #{remote.inspect}"
             synchro = R10K::Synchro::Git.new(remote)
             synchro.cache
           end
