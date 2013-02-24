2ddb1ba661f795b23dbd95ab7316623e74c3a460
Adrien Thebo <git@somethingsinistral.net>
2013-02-23 20:12:18 -0800

    Filter out integration tests by default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,9 @@ PROJECT_ROOT = File.expand_path('..', File.dirname(__FILE__))
 
 RSpec.configure do |config|
   config.mock_with :mocha
+
+  # Filter out integration tests by default
+  #
+  # To run integration tests, run `rspec --tag integration`
+  config.filter_run_excluding :integration => true
 end
