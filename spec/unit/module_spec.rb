425ca93d52ba045e01ce83c4e32177ecf062835e
Adrien Thebo <git@somethingsinistral.net>
2013-04-07 19:17:42 -0700

    Update tests for looser forge module matching

--- spec/unit/module_spec.rb
+++ spec/unit/module_spec.rb
@@ -2,20 +2,28 @@ require 'spec_helper'
 require 'r10k/module'
 
 describe R10K::Module do
-
-  it "should be able to delegate to R10K::Module::Git" do
-    obj = R10K::Module.new('foo', '/modulepath', :git => "git url")
-    obj.should be_a_kind_of R10K::Module::Git
+  describe 'delegating to R10K::Module::Git' do
+    it "accepts args {:git => 'git url}" do
+      obj = R10K::Module.new('foo', '/modulepath', :git => 'git url')
+      obj.should be_a_kind_of R10K::Module::Git
+    end
   end
 
-  it "should be able to delegate to R10K::Module::Forge" do
-    obj = R10K::Module.new('bar/quux', '/modulepath', '10.10.10')
-    obj.should be_a_kind_of R10K::Module::Forge
+  describe 'delegating to R10K::Module::Git' do
+    it "accepts name matching 'username/modulename' and no args" do
+      obj = R10K::Module.new('bar/quux', '/modulepath', [])
+      obj.should be_a_kind_of R10K::Module::Forge
+    end
+
+    it "accepts name matching 'username/modulename' and a semver argument" do
+      obj = R10K::Module.new('bar/quux', '/modulepath', '10.0.0')
+      obj.should be_a_kind_of R10K::Module::Forge
+    end
   end
 
-  it "should raise an exception if delegation fails" do
+  it "raises an error if delegation fails" do
     expect {
-      R10K::Module.new('bar/quux', '/modulepath', ["NOPE NOPE NOPE NOPE!"])
+      R10K::Module.new('bar-quux', '/modulepath', ["NOPE NOPE NOPE NOPE!"])
     }.to raise_error RuntimeError, /doesn't have an implementation/
   end
 end
