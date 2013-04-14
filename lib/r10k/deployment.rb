f9a5256b58415b71bd59a6f51a0676f1764b009e
Adrien Thebo <git@somethingsinistral.net>
2013-04-13 18:52:40 -0700

    Allow sources to list all environments

--- lib/r10k/deployment.rb
+++ lib/r10k/deployment.rb
@@ -1,6 +1,6 @@
 require 'r10k'
 require 'r10k/deployment/source'
-require 'r10k/config'
+require 'r10k/deployment/config'
 
 require 'yaml'
 
@@ -8,9 +8,8 @@ module R10K
 class Deployment
   # Model a full installation of module directories and modules.
 
-  def initialize
-    @config = R10K::Config.new
-    @config.load_config
+  def initialize(config)
+    @config = config
   end
 
   # @return [Array<R10K::Deployment::Source>] All sources for this deployment
@@ -23,6 +22,16 @@ class Deployment
     end
 
     sources
+  end
+
+  # @return [Array<R10K::Deployment::Environments>] All environments across all sources
+  def environments
+    envs = []
+    sources.each do |source|
+      source.fetch_environments
+      envs += source.environments
+    end
+    envs
   end
 end
 end
