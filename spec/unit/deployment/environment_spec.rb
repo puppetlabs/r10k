99121e558653d1ca6b841476acfb62c08a7aa23d
Adrien Thebo <git@somethingsinistral.net>
2013-04-14 01:08:22 -0700

    extend test stubs

--- spec/unit/deployment/environment_spec.rb
+++ spec/unit/deployment/environment_spec.rb
@@ -5,8 +5,15 @@ describe R10K::Deployment::Environment do
   let(:remote) { 'git://github.com/adrienthebo/r10k-fixture-repo' }
   let(:ref)    { 'master' }
 
-  it 'should use the ref as the default dirname' do
-    subject = described_class.new(ref, remote, '/tmp')
-    subject.dirname.should == 'master'
+  describe 'dirname' do
+    it 'uses the ref as the default dirname' do
+      subject = described_class.new(ref, remote, '/tmp')
+      subject.dirname.should == 'master'
+    end
+
+    it 'allows a specific dirname to be set' do
+      subject = described_class.new(ref, remote, '/tmp', 'sourcename_master')
+      subject.dirname.should == 'sourcename_master'
+    end
   end
 end
