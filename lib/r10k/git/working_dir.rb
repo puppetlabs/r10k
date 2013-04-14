ce659a179a2f166c3222eba78225f8fe2a8894f2
Adrien Thebo <git@somethingsinistral.net>
2013-04-14 14:44:26 -0700

    Refactor workingdir to be one object per working dir

--- lib/r10k/git/working_dir.rb
+++ lib/r10k/git/working_dir.rb
@@ -27,81 +27,81 @@ class WorkingDir
   #   @return [String] The URL to the git repository
   attr_reader :remote
 
-  # @todo
-  # @!attribute [r] path
-  #   @return [String] The path to the working directory
+  # @!attribute [r] basedir
+  #   @return [String] The basedir for the working directory
+  attr_reader :basedir
 
-  # @todo
+  # @!attribute [r] dirname
+  #   @return [String] The name for the directory
+  attr_reader :dirname
+
   # @!attribute [r] ref
   #   @return [String] The git reference to use check out in the given directory
+  attr_reader :ref
 
   # Instantiates a new git synchro and optionally prepares for caching
   #
-  # @param [String] remote A git remote URL
-  def initialize(remote)
-    @remote = remote
+  # @param [String] ref
+  # @param [String] remote
+  # @param [String] basedir
+  # @param [String] dirname
+  def initialize(ref, remote, basedir, dirname = nil)
+    @ref     = ref
+    @remote  = remote
+    @basedir = basedir
+    @dirname = dirname || ref
+
+    @full_path = File.join(@basedir, @dirname)
 
     @cache = R10K::Git::Cache.new(@remote)
   end
 
   # Synchronize the local git repository.
-  #
-  # @todo Remove the path and ref arguments and convert those into instance
-  #   variables to avoid mutating state.
-  #
-  # @param [String] path The destination path for the files
-  # @param [String] ref The git ref to instantiate at the destination path
-  def sync(path, ref, options = {:update_cache => true})
-    path = File.expand_path(path)
-    @cache.sync if options[:update_cache]
+  def sync
+    # TODO stop forcing a sync every time.
+    @cache.sync
 
-    if self.cloned?(path)
-      fetch(path)
+    if cloned?
+      fetch
     else
-      clone(path)
+      clone
     end
-    reset(path, ref)
+    reset
   end
 
   # Determine if repo has been cloned into a specific dir
   #
-  # @param [String] dirname The directory to check
-  #
   # @return [true, false] If the repo has already been cloned
-  def cloned?(directory)
-    File.directory?(File.join(directory, '.git'))
+  def cloned?
+    dot_git = File.join(@full_path, '.git')
+    File.directory? dot_git
   end
 
   private
 
   # Perform a non-bare clone of a git repository.
-  #
-  # @param [String] path The directory to create the repo working directory
-  def clone(path)
+  def clone
     # We do the clone against the target repo using the `--reference` flag so
     # that doing a normal `git pull` on a directory will work.
-    git "clone --reference #{@cache.path} #{@remote} #{path}"
-    git "remote add cache #{@cache.path}", :path => path
+    git "clone --reference #{@cache.path} #{@remote} #{@full_path}"
+    git "remote add cache #{@cache.path}", :path => @full_path
   end
 
-  def fetch(path)
+  def fetch
     # XXX This is crude but it'll ensure that the right remote is used for
     # the cache.
-    git "remote set-url cache #{@cache.path}", :path => path
-    git "fetch --prune cache", :path => path
+    git "remote set-url cache #{@cache.path}", :path => @full_path
+    git "fetch --prune cache", :path => @full_path
   end
 
   # Reset a git repo with a working directory to a specific ref
-  #
-  # @param [String] path The path to the working directory of the git repo
-  # @param [String] ref The git reference to reset to.
-  def reset(path, ref)
-    commit = resolve_commit(ref)
+  def reset
+    commit = resolve_commit(@ref)
 
     begin
-      git "reset --hard #{commit}", :path => path
+      git "reset --hard #{commit}", :path => @full_path
     rescue R10K::ExecutionFailure => e
-      logger.error "Unable to locate commit object #{commit} in git repo #{path}"
+      logger.error "Unable to locate commit object #{commit} in git repo #{@full_path}"
       raise
     end
   end
@@ -112,13 +112,12 @@ class WorkingDir
   #
   # @return [String] The dereferenced hash of `ref`
   def resolve_commit(ref)
-    commit = git "rev-parse #{ref}^{commit}", :git_dir => @cache.path
+    commit = git "rev-parse #{@ref}^{commit}", :git_dir => @cache.path
     commit.chomp
   rescue R10K::ExecutionFailure => e
-    logger.error "Could not resolve ref #{ref.inspect} for git cache #{@cache.path}"
+    logger.error "Could not resolve ref #{@ref.inspect} for git cache #{@cache.path}"
     raise
   end
-
 end
 end
 end
