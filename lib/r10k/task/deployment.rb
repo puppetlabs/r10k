176e1c459a55eef5dbb680cdd32804be415bcfe3
Adrien Thebo <git@somethingsinistral.net>
2013-04-21 20:25:05 -0700

    Update puppetfile tasks for deploying modules

--- lib/r10k/task/deployment.rb
+++ lib/r10k/task/deployment.rb
@@ -2,51 +2,12 @@ require 'r10k/task'
 require 'r10k/task_runner'
 
 require 'r10k/task/environment'
+require 'r10k/task/puppetfile'
 
 module R10K
 module Task
 module Deployment
-  class DeployEnvironments < R10K::Task::Base
-
-    # @!attrubte names
-    #   @return [Array<String>] A list of environments to deploy, by name.
-    attr_accessor :names
-
-    # @!attribute puppetfile
-    #   @return [TrueClass, FalseClass] Whether to deploy modules in a puppetfile
-    attr_accessor :puppetfile
-
-    def initialize(deployment, opts = {})
-      @deployment = deployment
-
-      @names      = opts.delete(:environments) || []
-      @puppetfile = opts.delete(:puppetfile)
-
-      raise "Unrecognized options: #{opts.keys.join(', ')}" unless opts.empty?
-    end
-
-    def call
-      logger.info "Loading environments from all sources"
-
-      load_environments!
-
-      # If an explicit list of environments were not given, deploy everything
-      if @names.size > 0
-        to_deploy = names
-      else
-        to_deploy = @environments.keys
-      end
-
-      to_deploy.each do |env_name|
-        if (env = @environments[env_name])
-          task = R10K::Task::Environment::Deploy.new(env, :puppetfile => @puppetfile)
-          task_runner.add_task task
-        else
-          logger.warn "Environment #{env_name} not found in any source"
-          task_runner.succeeded = false
-        end
-      end
-    end
+  module SharedBehaviors
 
     private
 
@@ -54,6 +15,84 @@ module Deployment
       @environments = @deployment.environments.inject({}) do |hash, env|
         hash[env.dirname] = env
         hash
+      end
+    end
+
+    # @param [Array<String>] names The list of environments to deploy.
+    #
+    def with_environments(names, &block)
+      load_environments!
+
+      # If an explicit list of environments were not given, deploy everything
+      if names.size > 0
+        to_deploy = names
+      else
+        to_deploy = @environments.keys
+      end
+
+      to_deploy.each do |env_name|
+        if (env = @environments[env_name])
+          yield env
+        else
+          logger.warn "Environment #{env_name} not found in any source"
+          task_runner.succeeded = false
+        end
+      end
+    end
+  end
+
+  class DeployEnvironments < R10K::Task::Base
+
+    include SharedBehaviors
+
+    # @!attribute environment_names
+    #   @return [Array<String>] A list of environments to deploy, by name.
+    attr_accessor :environment_names
+
+    # @!attribute update_puppetfile
+    #   @return [TrueClass, FalseClass] Whether to deploy modules in a puppetfile
+    attr_accessor :update_puppetfile
+
+    def initialize(deployment)
+      @deployment = deployment
+      @update_puppetfile = false
+      @environment_names = []
+    end
+
+    def call
+      logger.info "Loading environments from all sources"
+
+      with_environments(@environment_names) do |env|
+        task = R10K::Task::Environment::Deploy.new(env)
+        task.update_puppetfile = @update_puppetfile
+        task_runner.add_task task
+      end
+    end
+  end
+
+  class DeployModules < R10K::Task::Base
+
+    include SharedBehaviors
+
+    attr_accessor :module_names
+
+    # @!attribute environment_names
+    #   @return [Array<String>] A list of environments to update modules
+    attr_accessor :environment_names
+
+    def initialize(deployment)
+      @deployment        = deployment
+      @environment_names = []
+    end
+
+    def call
+      with_environments(@environment_names) do |env|
+        puppetfile = env.puppetfile
+
+        task = R10K::Task::Puppetfile::DeployModules.new(puppetfile)
+        task.module_names = module_names
+
+        task_runner.add_task task
       end
     end
   end
--- lib/r10k/task/puppetfile.rb
+++ lib/r10k/task/puppetfile.rb
@@ -21,6 +21,54 @@ module Puppetfile
       end
     end
   end
+
+  class DeployModules < R10K::Task::Base
+
+    attr_accessor :module_names
+
+    def initialize(puppetfile)
+      @puppetfile = puppetfile
+    end
+
+    def call
+      logger.info "Updating module list for Puppetfile #{@puppetfile.root}"
+      @puppetfile.load
+      load_modulemap!
+
+      existing = @modulemap.keys
+
+      warn_on_missing(existing, @module_names)
+
+      to_deploy = existing & @module_names
+
+      to_deploy.each do |mod_name|
+        mod = @modulemap[mod_name]
+        task = R10K::Task::Module::Sync.new(mod)
+        task_runner.add_task task
+      end
+    end
+
+    private
+
+    def warn_on_missing(existing, requested)
+      missing_modules = requested - existing
+
+      unless missing_modules.empty?
+        task_runner.succeeded = false
+
+        missing_modules.each do |missing|
+          logger.warn "Unable to deploy module #{missing}: not listed in Puppetfile"
+        end
+      end
+    end
+
+    def load_modulemap!
+        @modulemap = @puppetfile.modules.inject({}) do |hash, mod|
+        hash[mod.name] = mod
+        hash
+      end
+    end
+  end
 end
 end
 end
