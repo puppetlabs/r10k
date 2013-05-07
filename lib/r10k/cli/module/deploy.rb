9781b099ab8fc60daaf5659acd7178bc1d0926dd
Adrien Thebo <git@somethingsinistral.net>
2013-05-06 23:05:45 -0700

    Deprecate `r10k module deploy` command

--- lib/r10k/cli/module/deploy.rb
+++ lib/r10k/cli/module/deploy.rb
@@ -16,37 +16,9 @@ module R10K::CLI::Module
         flag :u, :update, "Update module cache"
 
         run do |opts, args, cmd|
+          logger.warn "This command is deprecated; please use `r10k deploy environment`"
 
-          unless (module_name = args[0])
-            puts cmd.help
-            exit 1
-          end
-
-          env_list = R10K::Deployment.instance.environments
-
-          if opts[:environment]
-            environments = env_list.select {|env| env.name == opts[:environment]}
-          else
-            environments = env_list
-          end
-
-          environments.each do |env|
-            mods = env.modules.select { |mod| mod.name == module_name }
-
-            if mods.empty?
-              logger.warn "No modules with name #{module_name} matched in environment #{env.name.inspect}".red
-            end
-
-            stack = Middleware::Builder.new
-            mods.each { |mod| stack.use R10K::Action::Module::Deploy, mod }
-
-            stack_env = {
-              :update_cache => opts[:update],
-              :trace        => opts[:trace],
-            }
-
-            stack.call(stack_env)
-          end
+          R10K::CLI::Deploy::Module.command.block.call(opts,args,cmd)
         end
       end
     end
