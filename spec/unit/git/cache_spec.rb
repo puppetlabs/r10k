902598130e59d1a206b3d93cde5447fbfca62daf
Justin Lambert <jlambert@eml.cc>
2013-09-11 08:08:19 -0600

    update spec tests

--- spec/unit/git/cache_spec.rb
+++ spec/unit/git/cache_spec.rb
@@ -2,16 +2,9 @@ require 'spec_helper'
 
 describe R10K::Git::Cache, 'memoizing objects' do
 
-  it "returns the same object when a remote is duplicated" do
-    first  = described_class.new('foo')
-    second = described_class.new('foo')
-
-    first.should be second
-  end
-
   it "wipes the memoized objects when .clear! is called" do
     first = described_class.new('foo')
-    described_class.clear!
+    described_class.registry.clear!
     second = described_class.new('foo')
 
     first.should_not be second
@@ -19,10 +12,6 @@ describe R10K::Git::Cache, 'memoizing objects' do
 end
 
 describe R10K::Git::Cache do
-
-  after do
-    described_class.clear!
-  end
 
   describe 'setting the cache root' do
     it 'defaults to ~/.r10k/git' do
