e07c2dcfe22dc121b306baac416028afe51e2c70
Adrien Thebo <git@somethingsinistral.net>
2013-09-13 12:13:50 -0700

    [registry] Add method documentation

--- lib/r10k/registry.rb
+++ lib/r10k/registry.rb
@@ -1,15 +1,30 @@
 module R10K
+
+  # R10K::Registry implements a generic object memoization container. It caches
+  # new objects and returns cached objects based on the instantiation arguments.
   class Registry
+
+    # Initialize a new registry with a given class
+    #
+    # @param klass [Class] The class to memoize
+    # @param method [Symbol] The method name to use when creating objects.
+    #                        Defaults to :new.
     def initialize(klass, method = :new)
       @klass  = klass
       @method = method
       @instances = {}
     end
 
+    # Create a new object, or return a memoized object.
+    #
+    # @param args [*Object] The arguments to pass to the initialize method
+    #
+    # @return [Object] A memoized instance of the registered class
     def generate(*args)
       @instances[args] ||= @klass.send(@method, *args)
     end
 
+    # Clear all memoized objects
     def clear!
       @instances = {}
     end
