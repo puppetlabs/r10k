045ef491b0f425916b1c99c98bd9287a880699b9
Adrien Thebo <git@somethingsinistral.net>
2013-09-12 17:18:01 -0700

    Switch to rspec-mocks for mocking framework

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,8 @@
 require 'r10k'
-require 'mocha'
 
 PROJECT_ROOT = File.expand_path('..', File.dirname(__FILE__))
 
 RSpec.configure do |config|
-  config.mock_with :mocha
-
   # Filter out integration tests by default
   #
   # To run integration tests, run `rspec --tag integration`
--- spec/unit/git/working_dir_spec.rb
+++ spec/unit/git/working_dir_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe R10K::Git::WorkingDir do
 
-  before do
-    R10K::Git::Cache.stubs(:cache_root).returns '/tmp'
-  end
-
   describe "initializing" do
     it "generates a new cache for the remote" do
       wd = described_class.new('master', 'git://github.com/adrienthebo/r10k-fixture-repo', '/tmp')
