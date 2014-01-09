a7559909ce600e5c14ffd388c5e06f64ca8634d6
Adrien Thebo <git@somethingsinistral.net>
2014-01-08 23:39:15 -0800

    [module/svn] Add more system spec coverage

--- spec/system/module/svn/install_spec.rb
+++ spec/system/module/svn/install_spec.rb
@@ -28,4 +28,52 @@ describe 'installing modules from SVN' do
       shell %[rm -r modules]
     end
   end
+
+  describe 'when a revision is specified' do
+    before(:all) do
+      shell %[rm -r modules]
+      shell %[echo "mod 'gitolite', :svn => 'https://github.com/nvalentine-puppetlabs/puppet-gitolite/trunk', :rev => '10'" > ./Puppetfile]
+    end
+
+    it "installs the module successfully" do
+      shell %[r10k puppetfile install] do |sh|
+        expect(sh.exit_code).to eq 0
+      end
+    end
+
+    it "creates the svn module" do
+      expect(file('modules/gitolite/.svn')).to be_directory
+    end
+
+    it "checks out the specific revision" do
+      expect(command('cd modules/gitolite; svn info')).to return_stdout /Revision: 10/
+    end
+
+    after(:all) do
+      shell %[rm -r modules]
+    end
+  end
+
+  describe 'updating to a specific revision' do
+    before(:all) do
+      shell %[rm -r modules]
+      shell %[echo "mod 'gitolite', :svn => 'https://github.com/nvalentine-puppetlabs/puppet-gitolite/trunk', :rev => '10'" > ./Puppetfile]
+      shell %[r10k puppetfile install]
+      shell %[echo "mod 'gitolite', :svn => 'https://github.com/nvalentine-puppetlabs/puppet-gitolite/trunk', :rev => '20'" > ./Puppetfile]
+    end
+
+    it "installs the module successfully" do
+      shell %[r10k puppetfile install] do |sh|
+        expect(sh.exit_code).to eq 0
+      end
+    end
+
+    it "checks out the specific revision" do
+      expect(command('cd modules/gitolite; svn info')).to return_stdout /Revision: 20/
+    end
+
+    after(:all) do
+      shell %[rm -r modules]
+    end
+  end
 end
