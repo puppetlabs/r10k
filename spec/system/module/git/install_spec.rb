021d88476d64b2cfc67a34a4e8d76b86f63f2b26
Adrien Thebo <git@somethingsinistral.net>
2014-01-19 22:53:42 -0800

    [spec/system] stub out git module tests

---
+++ spec/system/module/git/install_spec.rb
@@ -0,0 +1,14 @@
+require 'system/spec_helper'
+
+describe 'installing modules from git' do
+
+  describe 'when no version is specified' do
+    it "defaults to 'master'"
+  end
+
+  it "can check out a ref"
+  it "can check out a tag"
+  it "can check out a commit"
+  it "can check out a head"
+
+end
