eae9c6cdf477d225abde1430075dd29ffd1b91e5
Adrien Thebo <git@somethingsinistral.net>
2014-01-26 19:33:49 -0800

    [errors] Rename message ivar to match exception implementation

--- lib/r10k/errors.rb
+++ lib/r10k/errors.rb
@@ -5,24 +5,24 @@ module R10K
 
   # An error class that accepts an optional hash.
   #
-  # @overload initialize(message)
-  #   @param message [String] The exception message
+  # @overload initialize(mesg)
+  #   @param mesg [String] The exception mesg
   #
-  # @overload initialize(message, options)
-  #   @param message [String] The exception message
+  # @overload initialize(mesg, options)
+  #   @param mesg [String] The exception mesg
   #   @param options [Hash] A set of options to store on the exception
   #
   # @overload initialize(options)
   #   @param options [Hash] A set of options to store on the exception
   #
   class R10KError < StandardError
-    def initialize(message = nil, options = {})
-      if message.is_a? String
-        super(message)
+    def initialize(mesg = nil, options = {})
+      if mesg.is_a? String
+        super(mesg)
         @options = options
-      elsif message.is_a? Hash
-        @options = message
-        message = nil
+      elsif mesg.is_a? Hash
+        @options = mesg
+        @mesg = nil
       end
     end
   end
