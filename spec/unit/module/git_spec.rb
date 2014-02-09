020d415edb2e58eb7c33ccd7441a7e6ddcfb67b3
Adrien Thebo <git@somethingsinistral.net>
2014-02-08 21:45:16 -0800

    [module/git] Replace existing modules
    This addresses GH-30.

--- spec/unit/module/git_spec.rb
+++ spec/unit/module/git_spec.rb
@@ -19,17 +19,30 @@ describe R10K::Module::Git do
       expect(subject.status).to eq :absent
     end
 
-    it "is mismatched then the working dir is not a git repository" do
+    it "is mismatched Then the working dir is not a git repository" do
       allow(subject.working_dir).to receive(:exist?).and_return true
       expect(subject.working_dir).to receive(:git?).and_return false
       expect(subject.status).to eq :mismatched
     end
 
-    it "is outdated when the expected version doesn't match the actual version"
+    it "is mismatched when the expected remote does not match the actual remote" do
+      allow(subject.working_dir).to receive(:exist?).and_return true
+      expect(subject.working_dir).to receive(:git?).and_return true
+      expect(subject.working_dir).to receive(:remote).and_return 'nope'
+      expect(subject.status).to eq :mismatched
+    end
+
+    it "is outdated when the working dir is outdated" do
+      allow(subject.working_dir).to receive(:exist?).and_return true
+      expect(subject.working_dir).to receive(:git?).and_return true
+      expect(subject.working_dir).to receive(:outdated?).and_return true
+      expect(subject.status).to eq :outdated
+    end
 
     it "is insync if all other conditions are satisfied" do
       allow(subject.working_dir).to receive(:exist?).and_return true
-      allow(subject.working_dir).to receive(:git?).and_return true
+      expect(subject.working_dir).to receive(:git?).and_return true
+      expect(subject.working_dir).to receive(:outdated?).and_return false
       expect(subject.status).to eq :insync
     end
   end
