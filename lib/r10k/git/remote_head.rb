51df449fc9be23c43339d03b63ca3b15ba729507
Adrien Thebo <git@somethingsinistral.net>
2014-02-18 21:55:17 -0800

    Stub out remote head

---
+++ lib/r10k/git/remote_head.rb
@@ -0,0 +1,13 @@
+require 'r10k/git'
+
+# @api private
+class R10K::Git::RemoteHead < R10K::Git::Head
+
+  def sha1
+    if @repository.nil?
+      raise ArgumentError, "Cannot resolve #{self.inspect}: no associated git repository"
+    else
+      @repository.resolve_head("refs/remotes/cache/#{@head}")
+    end
+  end
+end
