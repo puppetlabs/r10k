d5bc0e08e74bfd5bfd502cedea4ba9f6bb323c9a
Adrien Thebo <git@somethingsinistral.net>
2014-02-28 12:12:35 -0800

    (maint) Fixup specs for 1.8.7

--- spec/unit/git/repository_spec.rb
+++ spec/unit/git/repository_spec.rb
@@ -15,10 +15,10 @@ describe R10K::Git::Repository do
     it "generates a hash of remote names and remote URLs" do
       expect(subject).to receive(:git).with(%w[remote -v], anything).and_return(double(:stdout => output))
 
-      expect(subject.remotes).to eq(
+      expect(subject.remotes).to eq({
         'cache'  => '/home/user/.r10k/git/git@github.com-org-org-modules',
         'origin' => 'git@github.com:org/org-modules',
-      )
+      })
     end
   end
 end
