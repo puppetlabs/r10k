18cc543f89fcc46e0584d31a8bec000e4fa2d021
Adrien Thebo <git@somethingsinistral.net>
2014-04-15 16:51:31 -0700

    [util/platform] Add windows/posix platform detection

---
+++ lib/r10k/util/platform.rb
@@ -0,0 +1,19 @@
+require 'rbconfig'
+
+module R10K
+  module Util
+    module Platform
+      def self.platform
+        if self.windows?
+          :windows
+        else
+          :posix
+        end
+      end
+
+      def self.windows?
+        RbConfig::CONFIG['host_os'] =~ /mswin|win32|dos|mingw|cygwin/i
+      end
+    end
+  end
+end
