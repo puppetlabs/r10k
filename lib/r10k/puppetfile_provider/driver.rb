5756a9ddf3dc2ae46465748b874caaf8b2efc0e5
Ryan Doyle <ryan@doylenet.net>
2014-04-20 17:18:15 +1000

    Clean up base driver so puppetfile_path always exists

--- lib/r10k/puppetfile_provider/driver.rb
+++ lib/r10k/puppetfile_provider/driver.rb
@@ -10,15 +10,17 @@ module PuppetfileProvider
 
     include R10K::Logging
 
+    PUPPET_FILE = "Puppetfile"
+
     def initialize(basedir, moduledir = nil, puppetfile_path = nil)
       @basedir = basedir
       @moduledir = moduledir
-      @puppetfile_path = puppetfile_path
+      @puppetfile_path = puppetfile_path || "#{basedir}/#{PUPPET_FILE}"
     end
 
     # TODO: Remove the same logic from R10K::Puppetfile
     def puppetfile_exists?
-      @puppetfile_path ? File.exists?(@puppetfile_path) : File.exists?("#{@basedir}/Puppetfile")
+      File.exists?(@puppetfile_path)
     end
 
   end
