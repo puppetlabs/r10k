d07f209b7e374e0defcd67547aa7deb65c8118fd
Adrien Thebo <git@somethingsinistral.net>
2014-04-20 18:41:45 -0700

    [subprocess/windows] (GH-100) Implement command runner
    This adds command execution for Windows platforms. The implementation
    relies on Open3.capture3 to run the command and collect the results. In
    contrast to the POSIX runner this cannot be used in an asynchronous
    manner as-is; implementing that will probably mean launching a thread
    and invoking #capture3 in that thread.

--- lib/r10k/util/subprocess/windows/runner.rb
+++ lib/r10k/util/subprocess/windows/runner.rb
@@ -1,2 +1,26 @@
+require 'open3'
+
 class R10K::Util::Subprocess::Windows::Runner < R10K::Util::Subprocess::Runner
+
+  def initialize(argv)
+    @argv = argv
+    @io = R10K::Util::Subprocess::Windows::IO.new
+  end
+
+  def run
+    cmd = @argv.join(' ')
+
+    stdout, stderr, status = Open3.capture3(cmd)
+
+    @status = status
+    @result = R10K::Util::Subprocess::Result.new(@argv, stdout, stderr, status.exitstatus)
+  end
+
+  def exit_code
+    @status.exitstatus
+  end
+
+  def crashed?
+    exit_code != 0
+  end
 end
