5babd18e274f565e86825cd03889551e0cacfef3
Christophe Bliard <christophe.bliard@smartesting.com>
2014-05-27 09:21:59 +0200

    Support moduledir with relative path
    When using the moduledir directive in a Puppetfile, the path is now
    relative to the repository basedir, except if it begins with a slash.

---
+++ spec/unit/puppetfile_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'r10k/puppetfile'
+
+describe R10K::Puppetfile do
+
+  subject do
+    described_class.new(
+      '/some/nonexistent/basedir',
+    )
+  end
+
+  describe "the default moduledir" do
+    it "is the basedir joined with '/modules' path" do
+      expect(subject.moduledir).to eq '/some/nonexistent/basedir/modules'
+    end
+  end
+
+  describe "setting moduledir" do
+    it "changes to given moduledir if it is an absolute path" do
+      subject.set_moduledir('/absolute/path/moduledir')
+      expect(subject.moduledir).to eq '/absolute/path/moduledir'
+    end
+
+    it "joins the basedir with the given moduledir if it is a relative path" do
+      subject.set_moduledir('relative/moduledir')
+      expect(subject.moduledir).to eq '/some/nonexistent/basedir/relative/moduledir'
+    end
+  end
+end
