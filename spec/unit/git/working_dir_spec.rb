puppetlabs/r10k

commit 3b9b39265d776ab8436f27655abcab3dd77d0c45

[git/working_dir] Simplify cache remote checking

lib/r10k/git/working_dir.rb
@@ -72,12 +72,13 @@ class R10K::Git::WorkingDir < R10K::Git::Repository
 
   private
 
+  def fetch
+    set_cache_remote
+    git "fetch --prune cache", :path => @full_path
+  end
+
   def set_cache_remote
-    # XXX This is crude but it'll ensure that the right remote is used for
-    # the cache.
-    if remote_url('cache') == @cache.path
-      logger.debug1 "Git repo #{@full_path} cache remote already set correctly"
-    else
+    if self.remote != @cache.remote
       git "remote set-url cache #{@cache.path}", :path => @full_path
     end
   end
@@ -89,11 +90,6 @@ class R10K::Git::WorkingDir < R10K::Git::Repository
     git "clone --reference #{@cache.path} #{@remote} #{@full_path}"
     git "remote add cache #{@cache.path}", :path => @full_path
     git "checkout #{@ref}", :path => @full_path
-  end
-
-  def fetch
-    set_cache_remote
-    git "fetch --prune cache", :path => @full_path
   end
 
   # Reset a git repo with a working directory to a specific ref
@@ -125,24 +121,6 @@ class R10K::Git::WorkingDir < R10K::Git::Repository
   rescue R10K::ExecutionFailure => e
     logger.error "Could not resolve ref #{ref.inspect} for git repo #{@full_path}"
     raise
-  end
-
-  # @param [String] name The remote to retrieve the URl for
-  # @return [String] The git remote URL
-  def remote_url(remote_name)
-    output = git "remote -v", :path => @full_path
-
-    remotes = {}
-
-    output.each_line do |line|
-      if mdata = line.match(/^(\S+)\s+(\S+)\s+\(fetch\)/)
-        name   = mdata[1]
-        remote = mdata[2]
-        remotes[name] = remote
-      end
-    end
-
-    remotes[remote_name]
   end
 
   def git_dir

commit c0eb0e918a95d4be9526478caf41060907689e00

[git/working_dir] (GH-137) Update remotes and alternates
Commit 3b9b3926 introduced a regression where the actual git remotes
were not inspected and the remote would always behave like it was in
sync. This meant that when a git module was switched from one remote to
another that change would not persist to the system. In addition the git
object alternates was not updated with the new cache, so the new repo
could start accumulating git objects unnecessarily.

This commit updates the working dir to inspect the git remotes for
'origin' and 'cache' to make sure they are set to the expected values,
and will update them as necessary when they are not properly set. Since
changing the origin remote means the cache is also updated the remotes
are always updated in pairs.

lib/r10k/git/working_dir.rb
@@ -41,7 +41,8 @@ class R10K::Git::WorkingDir < R10K::Git::Repository
     @full_path = File.join(@basedir, @dirname)
     @git_dir   = File.join(@full_path, '.git')
 
-    @cache = R10K::Git::Cache.generate(@remote)
+    @alternates = R10K::Git::Alternates.new(@git_dir)
+    @cache      = R10K::Git::Cache.generate(@remote)
 
     if ref.is_a? String
       @ref = R10K::Git::Ref.new(ref, self)
@@ -61,7 +62,9 @@ class R10K::Git::WorkingDir < R10K::Git::Repository
   end
 
   def update
-    if fetch?
+    update_remotes if update_remotes?
+
+    if ref_needs_fetch?
       fetch_from_cache
       checkout(@ref)
     elsif needs_checkout?
@@ -114,20 +117,15 @@ class R10K::Git::WorkingDir < R10K::Git::Repository
 
   private
 
-  def fetch?
+  # Do we need to fetch additional objects and refs in order to resolve the given ref?
+  # @return [true, false]
+  def ref_needs_fetch?
     @ref.fetch?
   end
 
   def fetch_from_cache
-    set_cache_remote
     @cache.sync
     fetch('cache')
-  end
-
-  def set_cache_remote
-    if self.remote != @cache.remote
-      git ["remote", "set-url", "cache", @cache.git_dir], :path => @full_path
-    end
   end
 
   # Perform a non-bare clone of a git repository.
@@ -151,6 +149,22 @@ class R10K::Git::WorkingDir < R10K::Git::Repository
     expected = ref.sha1
     actual   = rev_parse('HEAD')
 
-    ! (expected == actual)
+    !(expected == actual)
+  end
+
+  def update_remotes?
+    real_remotes = remotes
+
+    expected_origin = @remote
+    expected_cache  = @cache.git_dir
+
+    !(expected_origin == real_remotes['origin'] and expected_cache == real_remotes['cache'])
+  end
+
+  def update_remotes
+    # todo: remove all existing refs as they may belong to the old remote
+    git ['remote', 'set-url', 'origin', remote], :path => @full_path
+    git ['remote', 'set-url', 'cache', @cache.git_dir], :path => @full_path
+    @alternates << File.join(@cache.git_dir, 'objects')
   end
 end

spec/unit/git/working_dir_spec.rb
@@ -2,11 +2,120 @@ require 'spec_helper'
 require 'r10k/git'
 
 describe R10K::Git::WorkingDir do
+  include_context "fail on execution"
 
   describe "initializing" do
     it "generates a new cache for the remote" do
-      wd = described_class.new('master', 'git://github.com/adrienthebo/r10k-fixture-repo', '/tmp')
+      wd = described_class.new('master', 'git://github.com/adrienthebo/r10k-fixture-repo', '/some/nonexistent/dir')
       wd.cache.should be_kind_of R10K::Git::Cache
+    end
+
+    it "uses the provided ref as the dirname when no dirname is given" do
+      wd = described_class.new('master', 'git://github.com/adrienthebo/r10k-fixture-repo', '/some/nonexistent/dir')
+      expect(wd.dirname).to eq('master')
+    end
+
+    it "uses an explicit dirname when given" do
+      wd = described_class.new('master', 'git://github.com/adrienthebo/r10k-fixture-repo', '/some/nonexistent/dir', 'mydir')
+      expect(wd.dirname).to eq('mydir')
+    end
+  end
+
+  describe "synchronizing the working directory" do
+    subject { described_class.new('master', 'git://github.com/adrienthebo/r10k-fixture-repo', '/some/nonexistent/dir') }
+    it "clones the repository when the repository doesn't exist" do
+      expect(subject).to receive(:cloned?).and_return false
+      expect(subject).to receive(:clone)
+      subject.sync
+    end
+
+    it "updates the repository when the repository already exists" do
+      expect(subject).to receive(:cloned?).and_return true
+      expect(subject).to receive(:update)
+      subject.sync
+    end
+  end
+
+  describe "when cloning a new repository" do
+    subject { described_class.new('master', 'git://github.com/adrienthebo/r10k-fixture-repo', '/some/nonexistent/dir') }
+
+    before do
+      allow(subject).to receive(:cloned?).and_return false
+    end
+
+    it "updates the cache before cloning" do
+      expect(subject.cache).to receive(:sync)
+      allow(subject).to receive(:git)
+      allow(subject).to receive(:checkout)
+      subject.sync
+    end
+
+    it "clones the repository and uses the cache git dir as an object reference" do
+      allow(subject.cache).to receive(:sync)
+      expect(subject).to receive(:git).with(['clone', '--reference', subject.cache.git_dir,
+                                             'git://github.com/adrienthebo/r10k-fixture-repo',
+                                             '/some/nonexistent/dir/master'])
+      expect(subject).to receive(:git).with(['remote', 'add', 'cache', subject.cache.git_dir],
+                                            an_instance_of(Hash))
+
+      expect(subject).to receive(:git).with(['fetch', 'cache'], an_instance_of(Hash))
+      allow(subject).to receive(:checkout)
+      subject.sync
+    end
+
+    it 'checks out the specific ref after the clone' do
+      allow(subject.cache).to receive(:sync)
+      allow(subject).to receive(:git)
+      expect(subject).to receive(:checkout)
+      subject.sync
+    end
+  end
+
+  describe "updating an existing repository" do
+    subject { described_class.new('master', 'git://github.com/adrienthebo/r10k-fixture-repo', '/some/nonexistent/dir') }
+
+    before do
+      allow(subject).to receive(:cloned?).and_return true
+    end
+
+    it "updates the remotes when they are out of sync" do
+      allow(subject).to receive(:ref_needs_fetch?).and_return false
+      allow(subject).to receive(:needs_checkout?).and_return false
+
+      expect(subject).to receive(:update_remotes?).and_return true
+      expect(subject).to receive(:update_remotes)
+
+      subject.sync
+    end
+
+    it "updates the cache when the ref requires an update" do
+      allow(subject).to receive(:update_remotes?).and_return false
+
+      expect(subject).to receive(:ref_needs_fetch?).and_return true
+      expect(subject).to receive(:fetch_from_cache)
+      expect(subject).to receive(:checkout).with(an_instance_of(R10K::Git::Ref))
+
+      subject.sync
+    end
+
+    it "checks out the ref when the wrong commit is checked out" do
+      allow(subject).to receive(:update_remotes?).and_return false
+      allow(subject).to receive(:ref_needs_fetch?).and_return false
+
+      expect(subject).to receive(:needs_checkout?).and_return true
+      expect(subject).to receive(:checkout).with(an_instance_of(R10K::Git::Ref))
+
+      subject.sync
+    end
+
+    it "doesn't update the repo when everything is in sync" do
+      allow(subject).to receive(:update_remotes?).and_return false
+      allow(subject).to receive(:ref_needs_fetch?).and_return false
+      allow(subject).to receive(:needs_checkout?).and_return false
+
+      expect(subject).to_not receive(:checkout)
+
+      subject.sync
     end
   end
 end