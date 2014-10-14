364fa69fb510f1afadd6ea888519c49d9420b081
Adrien Thebo <git@somethingsinistral.net>
2014-10-14 14:52:54 -0700

    [action/runner] prototype action runner

---
+++ lib/r10k/action/runner.rb
@@ -0,0 +1,27 @@
+module R10K
+  module Action
+    class Runner
+      def initialize(argv, opts, klass)
+        @argv = argv
+        @opts = opts
+        @klass = klass
+      end
+
+      def call
+        setup_logging
+        setup_settings
+        # check arguments
+        @klass.new(@argv, @opts).call
+      end
+
+      def setup_logging
+        if @opts[:verbose]
+          R10K::Logging.level = @opts.delete(:verbose)
+        end
+      end
+
+      def setup_settings
+      end
+    end
+  end
+end
