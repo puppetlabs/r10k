620f05e14a631efe0c8dca70ef8c0382a755cb85
Adrien Thebo <git@somethingsinistral.net>
2014-11-04 17:13:02 -0800

    [environment/base] Make environments visitable

--- spec/unit/environment/base_spec.rb
+++ spec/unit/environment/base_spec.rb
@@ -12,4 +12,27 @@ describe R10K::Environment::Base do
   it "raises an exception when #sync is called" do
     expect { environment.sync }.to raise_error(NotImplementedError)
   end
+
+  describe "accepting a visitor" do
+    it "passes itself to the visitor" do
+      visitor = spy('visitor')
+      expect(visitor).to receive(:visit).with(:environment, subject)
+      subject.accept(visitor)
+    end
+
+    it "passes the visitor to the puppetfile if the visitor yields" do
+      visitor = spy('visitor')
+      expect(visitor).to receive(:visit) do |type, other, &block|
+        expect(type).to eq :environment
+        expect(other).to eq subject
+        block.call
+      end
+
+      pf = spy('puppetfile')
+      expect(pf).to receive(:accept).with(visitor)
+
+      expect(subject).to receive(:puppetfile).and_return(pf)
+      subject.accept(visitor)
+    end
+  end
 end
