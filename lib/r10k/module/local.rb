fa9118a61ea208e333242f12d71da4e5bf183189
Kirill Kondratenko <cybem@cybem.info>
2015-02-22 02:47:18 +0300

    Add local module type

---
+++ lib/r10k/module/local.rb
@@ -0,0 +1,21 @@
+require 'r10k/module'
+
+class R10K::Module::Local < R10K::Module::Base
+
+  R10K::Module.register(self)
+
+  def self.implement?(name, args)
+    args.is_a? Symbol and args == :local
+  end
+
+  def initialize(name, dirname, opts)
+    super
+  end
+
+  def status
+    :insync
+  end
+
+  def sync
+  end
+end
