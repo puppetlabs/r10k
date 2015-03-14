401ab124b8c0c58ceff6f36e312fb3291258e8a8
Carlos Velarde <carlos.velarde@puppetlabs.com>
2015-03-13 18:07:55 -0700

    (CODEMGMT-117) Create Automated Tests for Sprint 2015-03-25
    Add two integration tests for User Scenario -
    Basic Workflow: Single Environment Specify Module
    that is Already Installed, Single Environment
    Upgrade Forge Module then Revert Change.

---
+++ integration/tests/user_scenario/basic_workflow/single_env_module_already_installed.rb
@@ -0,0 +1,73 @@
+require 'git_utils'
+require 'r10k_utils'
+require 'master_manipulator'
+test_name 'CODEMGMT-117 - C63601 - Single Environment Specify Module that is Already Installed' 
+
+#Init
+master_certname = on(master, puppet('config', 'print', 'certname')).stdout.rstrip
+environment_path = on(master, puppet('config', 'print', 'environmentpath')).stdout.rstrip
+prod_env_path = File.join(environment_path, 'production')
+git_environments_path = '/root/environments'
+last_commit = git_last_commit(master, git_environments_path)
+prod_env_path = File.join(environment_path, 'production')
+motd_module_init_pp_path = File.join(prod_env_path, 'modules/motd/manifests/init.pp')
+
+forge_module_path = File.join(prod_env_path, 'modules')
+
+#Verification
+motd_path = '/etc/motd'
+motd_contents = 'Hello!'
+motd_contents_regex = /\A#{motd_contents}\z/
+
+#File
+puppet_file = <<-PUPPETFILE
+mod "puppetlabs/motd"
+PUPPETFILE
+
+puppet_file_path = File.join(git_environments_path, 'Puppetfile')
+
+#Manifest
+manifest = <<-MANIFEST
+  class { 'motd':
+    content => '#{motd_contents}',
+}
+MANIFEST
+
+site_pp_path = File.join(git_environments_path, 'manifests', 'site.pp')
+site_pp = create_site_pp(master_certname, manifest)
+
+#Teardown
+teardown do
+  clean_up_r10k(master, last_commit, git_environments_path)
+
+  step 'Remove "/etc/motd" File'
+  on(agents, "rm -rf #{motd_path}")
+end
+
+#Setup
+step 'Add motd module from the forge using the PMT'
+on(master, puppet('module', 'install', 'puppetlabs-motd', '--modulepath', forge_module_path)) 
+
+step 'Checkout "production" Branch'
+git_on(master, 'checkout production', git_environments_path)
+
+step 'Inject New "site.pp" to the "production" Environment'
+inject_site_pp(master, site_pp_path, site_pp)
+
+step 'Create "Puppetfile" for the "production" Environment'
+create_remote_file(master, puppet_file_path, puppet_file)
+
+step 'Push Changes'
+git_add_commit_push(master, 'production', 'Update site.pp and add module.', git_environments_path)
+
+#Tests
+step 'Deploy "production" Environment via r10k with modules'
+on(master, 'r10k deploy environment -p -v')
+
+agents.each do |agent|
+  step "Run Puppet Agent"
+  on(agent, puppet('agent', '--test', '--environment production'), :acceptable_exit_codes => 1)do |result|
+    assert_match(/Error:/, result.stderr, 'Unexpected error was detected!')
+binding.pry
+  end
+end
