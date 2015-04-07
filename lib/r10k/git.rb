94bfeebc1e8f4a723187c60c78ab5fcde2e1bd39
Adrien Thebo <git@somethingsinistral.net>
2015-04-07 10:04:41 -0700

    [git] (RK-62) Warn if rugged is missing features
    If Rugged was compiled without SSH support and the SSH transport is
    used, Rugged will throw a rather cryptic error. This commit adds an
    `:on_set` hook for Git providers, uses that hook to check for the
    features compiled into Rugged, and emits a warning if the https or ssh
    feature is missing.

--- lib/r10k/git.rb
+++ lib/r10k/git.rb
@@ -1,18 +1,38 @@
 require 'r10k/features'
 require 'r10k/errors'
 require 'r10k/settings'
+require 'r10k/logging'
 
 module R10K
   module Git
     require 'r10k/git/shellgit'
     require 'r10k/git/rugged'
 
+    extend R10K::Logging
+
     # A list of Git providers, sorted by priority. Providers have features that
     # must be available for them to be used, and a module which is the namespace
     # containing the implementation.
     @providers = [
-      [:shellgit, {:feature => :shellgit, :module => R10K::Git::ShellGit}],
-      [:rugged,   {:feature => :rugged,   :module => R10K::Git::Rugged}],
+      [ :shellgit,
+        {
+          :feature => :shellgit,
+          :module  => R10K::Git::ShellGit,
+        }
+      ],
+      [ :rugged,
+        {
+          :feature => :rugged,
+          :module  => R10K::Git::Rugged,
+          :on_set  => proc do
+            [:ssh, :https].each do |transport|
+              if !::Rugged.features.include?(transport)
+                logger.warn "Rugged has been compiled without support for #{transport}; Git repositories will not be reachable via #{transport}."
+              end
+            end
+          end
+        }
+      ],
     ]
 
     # Mark the current provider as invalid.
@@ -39,14 +59,13 @@ module R10K
     # Return the first available Git provider.
     #
     # @raise [R10K::Error] if no Git providers are functional.
-    # @return [Module] The namespace of the first available Git implementation.
-    #   Implementation classes should be looked up against this returned Module.
-    def self.default
-      _, attrs = @providers.find { |(_, hash)| R10K::Features.available?(hash[:feature]) }
-      if attrs.nil?
+    # @return [String] The name of the first available Git implementation.
+    def self.default_name
+      name, _ = @providers.find { |(_, hash)| R10K::Features.available?(hash[:feature]) }
+      if name.nil?
         raise R10K::Error, "No Git providers are functional."
       end
-      attrs[:module]
+      name
     end
 
     # Manually set the Git provider by name.
@@ -65,6 +84,9 @@ module R10K
         @provider = NULL_PROVIDER
         raise R10K::Error, "Git provider '#{name}' is not functional."
       end
+      if attrs[:on_set]
+        attrs[:on_set].call
+      end
       @provider = attrs[:module]
     end
 
@@ -75,10 +97,9 @@ module R10K
       when NULL_PROVIDER
         raise R10K::Error, "No Git provider set."
       when UNSET_PROVIDER
-        @provider = default
-      else
-        @provider
+        self.provider = default_name
       end
+      @provider
     end
 
     def self.cache
