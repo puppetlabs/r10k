ae6a61a0c0215230f93022ada60d7173e1d64bf6
Ryan Gard <ryan.gard@puppetlabs.com>
2015-04-13 09:27:51 -0700

    (CODEMGMT-167) Exploratory Testing Helper Scripts
    Added several helper scripts for setting up an environment in the "vmpooler"
    for "r10k" exploratory testing. A README has been added to document how to
    use the scripts. Also, the Beaker configs have been updated to make it
    clear in Beaker output which machine is the Puppet master.

---
+++ integration/scripts/setup_r10k_env_centos5.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SCRIPT_PATH=$(pwd)
+BASENAME_CMD="basename ${SCRIPT_PATH}"
+SCRIPT_BASE_PATH=`eval ${BASENAME_CMD}`
+
+if [ $SCRIPT_BASE_PATH = "scripts" ]; then
+  cd ../
+fi
+
+export pe_dist_dir=http://neptune.puppetlabs.lan/3.8/ci-ready/
+export GIT_PROVIDER=shellgit
+
+bundle install --path .bundle/gems
+
+bundle exec beaker \
+  --preserve-hosts always \
+  --config configs/pe/centos-5-64mda \
+  --debug \
+  --keyfile ~/.ssh/id_rsa-acceptance \
+  --pre-suite pre-suite \
+  --load-path lib
+
+rm -rf .bundle
