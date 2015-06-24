a37de3f2e78ced4db5c980fcb292d4fa98faf1c9
Adrien Thebo <git@somethingsinistral.net>
2015-06-24 10:56:49 -0700

    [settings] Add collection for Git settings

--- lib/r10k/settings.rb
+++ lib/r10k/settings.rb
@@ -1,6 +1,34 @@
+require 'etc'
+
 module R10K
   module Settings
     require 'r10k/settings/container'
     require 'r10k/settings/mixin'
+
+    require 'r10k/settings/collection'
+    require 'r10k/settings/definition'
+
+    def self.git_settings
+      R10K::Settings::Collection.new(:git, [
+
+        EnumDefinition.new(:provider, {
+          :desc => "The Git provider to use. Valid values: 'shellgit', 'rugged'",
+          :normalize => lambda { |input| input.to_sym },
+          :enum => [:shellgit, :rugged],
+        }),
+
+        Definition.new(:username, {
+          :desc => "The username to use for Git SSH remotes that do not specify a user.
+                    Only used by the 'rugged' Git provider.
+                    Default: the current user",
+          :default => lambda { Etc.getlogin },
+        }),
+
+        Definition.new(:private_key, {
+          :desc => "The path to the SSH private key for Git SSH remotes.
+                    Only used by the 'rugged' Git provider.",
+        })
+      ])
+    end
   end
 end
