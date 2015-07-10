4feb3f127ed5b30a20bf624772658aebf6cd9d9c
Zach Reichert <zach.reichert@puppetlabs.com>
2015-07-10 08:55:28 -0700

    (RK-110) Tests for proxy support.

---
+++ integration/tests/basic_functionality/proxy_specified_in_configuration.rb
@@ -0,0 +1,105 @@
+require 'erb'
+require 'git_utils'
+require 'r10k_utils'
+require 'master_manipulator'
+test_name 'RK-110 - C63604 - Specify the proxy in the r1ok.yaml'
+
+confine(:to, :platform => ['el', 'sles'])
+
+#Init
+master_platform = fact_on(master, 'osfamily')
+env_path = on(master, puppet('config print environmentpath')).stdout.rstrip
+r10k_fqp = get_r10k_fqp(master)
+
+git_repo_path = '/git_repos'
+git_repo_name = 'environments'
+git_control_remote = File.join(git_repo_path, "#{git_repo_name}.git")
+git_environments_path = '/root/environments'
+last_commit = git_last_commit(master, git_environments_path)
+git_provider = ENV['GIT_PROVIDER']
+
+local_files_root_path = ENV['FILES'] || 'files'
+
+git_manifest_template_path = File.join(local_files_root_path, 'pre-suite', 'git_config.pp.erb')
+git_manifest = ERB.new(File.read(git_manifest_template_path)).result(binding)
+
+r10k_config_path = get_r10k_config_file_path(master)
+r10k_config_bak_path = "#{r10k_config_path}.bak"
+
+case master_platform
+  when 'RedHat'
+    pkg_manager = 'yum'
+    squid = 'squid'
+  when 'Suse'
+    pkg_manager = 'zypper'
+    squid = 'squid'
+end
+
+install_squid = "#{pkg_manager} install -y #{squid}"
+remove_squid = "#{pkg_manager} remove -y #{squid}"
+squid_log = "/var/log/#{squid}/access.log"
+
+#In-line files
+r10k_conf = <<-CONF
+cachedir: '/var/cache/r10k'
+git:
+  provider: '#{git_provider}'
+sources:
+  control:
+    basedir: "#{env_path}"
+    remote: "#{git_control_remote}"
+forge:
+  proxy: "http://#{master.hostname}:3128"
+CONF
+
+#Verification
+squid_log_regex = /CONNECT forgeapi.puppetlabs.com:443/
+
+#Teardown
+teardown do
+  step 'Restore "git" Package'
+  on(master, puppet('apply'), :stdin => git_manifest, :acceptable_exit_codes => [0,2])
+
+  step 'Restore Original "r10k" Config'
+  on(master, "mv #{r10k_config_bak_path} #{r10k_config_path}")
+
+  clean_up_r10k(master, last_commit, git_environments_path)
+
+  step 'Remove Squid'
+  on(master, puppet("apply -e 'service {'squid' : ensure => stopped}'"))
+  on(master, remove_squid)
+end
+
+#Setup
+step 'Backup Current "r10k" Config'
+on(master, "mv #{r10k_config_path} #{r10k_config_bak_path}")
+
+step 'Update the "r10k" Config'
+create_remote_file(master, r10k_config_path, r10k_conf)
+
+step 'Checkout "production" Branch'
+git_on(master, 'checkout production', git_environments_path)
+
+step 'Copy Puppetfile to "production" Environment Git Repo'
+create_remote_file(master, "#{git_environments_path}/Puppetfile", 'mod "puppetlabs/motd"')
+
+step 'Push Changes'
+git_add_commit_push(master, 'production', 'add Puppetfile', git_environments_path)
+
+step 'Install and configure squid proxy'
+on(master, install_squid)
+
+step 'turn off the firewall'
+on(master, puppet("apply -e 'service {'iptables' : ensure => stopped}'"))
+
+step 'start squid proxy'
+on(master, puppet("apply -e 'service {'#{squid}' : ensure => running}'"))
+
+#Tests
+step 'Deploy "production" Environment via r10k'
+on(master, "#{r10k_fqp} deploy environment -p")
+
+step 'Read the squid logs'
+on(master, "cat #{squid_log}") do |result|
+  assert_match(squid_log_regex, result.stdout, 'Proxy logs did not indicate use of the proxy.')
+end
---
+++ integration/tests/basic_functionality/proxy_with_puppetfile.rb
@@ -0,0 +1,60 @@
+require 'erb'
+require 'git_utils'
+require 'r10k_utils'
+require 'master_manipulator'
+test_name 'RK-110 - C87651 - Specify a proxy in an environment variable'
+
+confine(:to, :platform => ['el', 'sles'])
+
+#Init
+master_platform = fact_on(master, 'osfamily')
+r10k_fqp = get_r10k_fqp(master)
+
+case master_platform
+  when 'RedHat'
+    pkg_manager = 'yum'
+    squid = 'squid'
+  when 'Suse'
+    pkg_manager = 'zypper'
+    squid = 'squid'
+end
+
+install_squid = "#{pkg_manager} install -y #{squid}"
+remove_squid = "#{pkg_manager} remove -y #{squid}"
+squid_log = "/var/log/#{squid}/access.log"
+
+#Verification
+squid_log_regex = /CONNECT forgeapi.puppetlabs.com:443/
+
+#Teardown
+teardown do
+  step 'Remove puppetfile'
+  on(master, 'rm -rf modules/')
+  on(master, 'rm Puppetfile')
+
+  step 'Remove Squid'
+  on(master, puppet("apply -e 'service {'squid' : ensure => stopped}'"))
+  on(master, remove_squid)
+end
+
+step 'Install and configure squid proxy'
+on(master, install_squid)
+master.add_env_var('http_proxy', "http://#{master.hostname}:3128")
+
+step 'turn off the firewall'
+on(master, puppet("apply -e 'service {'iptables' : ensure => stopped}'"))
+
+step 'start squid proxy'
+on(master, puppet("apply -e 'service {'#{squid}' : ensure => running}'"))
+
+#Tests
+step 'make a puppetfile'
+create_remote_file(master, "Puppetfile", 'mod "puppetlabs/motd"')
+
+step 'Use a r10k puppetfile'
+on(master, "#{r10k_fqp} puppetfile install")
+
+step 'Read the squid logs'
+on(master, "cat #{squid_log}") do |result|
+  assert_match(squid_log_regex, result.stdout, 'Proxy logs did not indicate use of the proxy.')
+end
