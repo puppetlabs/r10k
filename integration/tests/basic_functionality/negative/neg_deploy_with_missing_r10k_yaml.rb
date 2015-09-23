899af735cae73b8995b297a12f1e2f22cc04c9a9
Zach Reichert <zach.reichert@puppetlabs.com>
2015-09-23 11:52:31 -0700

    (CODEMGMT-380) update regex for neg_deploy_with_missing_r10k_yaml

--- integration/tests/basic_functionality/negative/neg_deploy_with_missing_r10k_yaml.rb
+++ integration/tests/basic_functionality/negative/neg_deploy_with_missing_r10k_yaml.rb
@@ -9,7 +9,7 @@ r10k_config_bak_path = "#{r10k_config_path}.bak"
 r10k_fqp = get_r10k_fqp(master)
 
 #Verification
-error_message_regex = /Error while running.*R10K\:\:Deployment.*No configuration file given/
+error_message_regex = /No configuration file given, no config file found in current directory, and no global config present/
 
 #Teardown
 teardown do
