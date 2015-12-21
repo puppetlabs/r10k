50c9d1178ee93fbe1c6d596af784686223ff71ce
Jesse Scott <jesse@puppetlabs.com>
2015-12-21 12:50:03 -0800

    Work around some bugs in Popen3 that exist in older versions of JRuby.

--- lib/r10k/util/subprocess/runner/jruby.rb
+++ lib/r10k/util/subprocess/runner/jruby.rb
@@ -1,5 +1,7 @@
 require 'open3'
 require 'r10k/util/subprocess/runner'
+require 'r10k/util/subprocess/result'
+require 'r10k/logging'
 
 # Run processes under JRuby.
 #
@@ -8,14 +10,16 @@ require 'r10k/util/subprocess/runner'
 # asynchronous manner as-is; implementing that will probably mean launching a
 # thread and invoking #capture3 in that thread.
 class R10K::Util::Subprocess::Runner::JRuby < R10K::Util::Subprocess::Runner
+  include R10K::Logging
 
   def initialize(argv)
     @argv = argv
   end
 
   def run
-    spawn_opts = @cwd ? {:chdir => @cwd} : {}
-    stdout, stderr, status = Open3.capture3(*@argv, spawn_opts)
+    logger.warn("Modifying working directory for subprocess is not supported under JRuby") if @cwd
+
+    stdout, stderr, status = Open3.capture3(@argv.join(' '))
     @result = R10K::Util::Subprocess::Result.new(@argv, stdout, stderr, status.exitstatus)
   rescue Errno::ENOENT, Errno::EACCES => e
     @result = R10K::Util::Subprocess::Result.new(@argv, '', e.message, 255)
