33f19eace7c56e7b5e0650f3fdb3cee524c8f44e
Jesse Scott <jesse@puppetlabs.com>
2015-12-21 12:50:03 -0800

    Work around some bugs in Popen3 that exist in older versions of JRuby.

--- lib/r10k/util/subprocess.rb
+++ lib/r10k/util/subprocess.rb
@@ -74,7 +74,7 @@ module R10K
         logger.debug2("Finished process:\n#{result.format}")
 
         if @raise_on_fail && result.failed?
-          raise SubprocessError.new("Command exited with non-zero exit code", :result => result)
+          raise SubprocessError.new("Command exited with non-zero exit code: #{result.stderr}", :result => result)
         end
 
         result
