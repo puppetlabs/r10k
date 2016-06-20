370419f3a92bcae017dbef89656ae2a96b5eb673
Zach Reichert <zach.reichert@puppetlabs.com>
2016-06-20 08:26:09 -0700

    (MAINT) update regex used in proxy tests

--- integration/tests/Puppetfile/HTTPS_PROXY_effects_git_source.rb
+++ integration/tests/Puppetfile/HTTPS_PROXY_effects_git_source.rb
@@ -64,7 +64,7 @@ git_add_commit_push(master, 'production', 'add Puppetfile', git_environments_pat
 
 #test
 on(master, "#{r10k_fqp} deploy environment -p", :accept_all_exit_codes => true) do |r|
-  regex = /Couldn't resolve proxy 'ferritsarebest\.net'/
+  regex = /(Couldn't|Could not) resolve proxy.*ferritsarebest\.net/i
   assert(r.exit_code == 1, 'expected error code was not observed')
   assert_match(regex, r.stderr, 'The expected error message was not observed' )
 end
