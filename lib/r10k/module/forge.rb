7b055c5e1ffdcffd4248bdd770392994666bc8b0
Patrick Robinson <patrick.robinson@envato.com>
2016-06-27 14:57:14 +1000

    When not passing a version args is nil

--- lib/r10k/module/forge.rb
+++ lib/r10k/module/forge.rb
@@ -17,7 +17,7 @@ class R10K::Module::Forge < R10K::Module::Base
   end
 
   def self.valid_version?(expected_version)
-    expected_version == :latest || expected_version.empty? || SemanticPuppet::Version.valid?(expected_version)
+    expected_version == :latest || expected_version.nil? || SemanticPuppet::Version.valid?(expected_version)
   end
 
   # @!attribute [r] metadata
