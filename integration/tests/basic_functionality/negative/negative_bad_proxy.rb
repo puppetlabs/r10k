7d0b8fe3f439055b47bf45dfc9cde0ef81653103
Jesse Scott <jesse@puppetlabs.com>
2016-08-03 13:40:06 -0700

    (MAINT) Change how proxy env is configured for bad proxy test.

--- integration/tests/basic_functionality/negative/negative_bad_proxy.rb
+++ integration/tests/basic_functionality/negative/negative_bad_proxy.rb
@@ -9,16 +9,6 @@ confine(:to, :platform => ['el', 'sles'])
 master_platform = fact_on(master, 'osfamily')
 r10k_fqp = get_r10k_fqp(master)
 
-case master_platform
-  when 'RedHat'
-    pkg_manager = 'yum'
-  when 'Suse'
-    pkg_manager = 'zypper'
-end
-
-install_squid = "#{pkg_manager} install -y squid"
-remove_squid = "#{pkg_manager} remove -y squid"
-
 #Verification
 proxy_hostname = "http://notarealhostname:3128"
 error_regex = /Unable to connect to.*#{proxy_hostname}/i
@@ -28,31 +18,17 @@ teardown do
   step 'Remove puppetfile'
   on(master, 'rm -rf modules/')
   on(master, 'rm Puppetfile')
-
-  step 'Remove Squid'
-  on(master, puppet("apply -e 'service {'squid' : ensure => stopped}'"))
-  on(master, remove_squid)
-
-  step 'Remove proxy environment variable'
-  master.delete_env_var('http_proxy', 'http://notarealhostname:3128')
 end
-
-step 'Install and configure squid proxy'
-on(master, install_squid)
-master.add_env_var('http_proxy', proxy_hostname)
 
 step 'turn off the firewall'
 on(master, puppet("apply -e 'service {'iptables' : ensure => stopped}'"))
-
-step 'start squid proxy'
-on(master, puppet("apply -e 'service {'squid' : ensure => running}'"))
 
 #Tests
 step 'make a puppetfile'
 create_remote_file(master, "Puppetfile", 'mod "puppetlabs/motd"')
 
 step 'Use a r10k puppetfile'
-on(master, "#{r10k_fqp} puppetfile install", {:acceptable_exit_codes => [0,1,2]}) do |result|
+on(master, "#{r10k_fqp} puppetfile install", {:acceptable_exit_codes => [0,1,2], :environment => {"http_proxy" => proxy_hostname}}) do |result|
   assert(result.exit_code == 1, 'The expected exit code was not observed.')
   assert_match(error_regex, result.stderr, 'Did not see the expected error')
 end
