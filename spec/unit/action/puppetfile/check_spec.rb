d9cd47c331d5cc0b3c33746d858af886488113fe
Jesse Scott <jesse@puppetlabs.com>
2016-12-16 11:57:20 -0800

    (MAINT) Update spec tests for puppetfile and moduledir options.

--- spec/unit/action/puppetfile/check_spec.rb
+++ spec/unit/action/puppetfile/check_spec.rb
@@ -2,25 +2,40 @@ require 'spec_helper'
 require 'r10k/action/puppetfile/check'
 
 describe R10K::Action::Puppetfile::Check do
+  let(:default_opts) { {root: "/some/nonexistent/path"} }
+  let(:puppetfile) { instance_double('R10K::Puppetfile', :load! => true) }
 
-  subject { described_class.new({root: "/some/nonexistent/path"}, []) }
+  def checker(opts = {}, argv = [], settings = {})
+    opts = default_opts.merge(opts)
+    return described_class.new(opts, argv, settings)
+  end
 
-  let(:puppetfile) { instance_double('R10K::Puppetfile') }
-
-  before { allow(R10K::Puppetfile).to receive(:new).with("/some/nonexistent/path", nil, nil).and_return(puppetfile) }
+  before(:each) do
+    allow(R10K::Puppetfile).to receive(:new).with("/some/nonexistent/path", nil, nil).and_return(puppetfile)
+  end
 
   it_behaves_like "a puppetfile action"
 
   it "prints 'Syntax OK' when the Puppetfile syntax could be validated" do
-    expect(puppetfile).to receive(:load!)
     expect($stderr).to receive(:puts).with("Syntax OK")
-    subject.call
+
+    checker.call
   end
 
   it "prints an error message when validating the Puppetfile syntax raised an error" do
-    expect(puppetfile).to receive(:load!).and_raise(R10K::Error.new("Boom!"))
-    expect(R10K::Errors::Formatting).to receive(:format_exception).with(instance_of(R10K::Error), anything).and_return("Formatted error message")
+    allow(puppetfile).to receive(:load!).and_raise(R10K::Error.new("Boom!"))
+    allow(R10K::Errors::Formatting).to receive(:format_exception).with(instance_of(R10K::Error), anything).and_return("Formatted error message")
+
     expect($stderr).to receive(:puts).with("Formatted error message")
-    subject.call
+
+    checker.call
+  end
+
+  it "respects --puppetfile option" do
+    allow($stderr).to receive(:puts)
+
+    expect(R10K::Puppetfile).to receive(:new).with("/some/nonexistent/path", nil, "/custom/puppetfile/path").and_return(puppetfile)
+
+    checker({puppetfile: "/custom/puppetfile/path"}).call
   end
 end
--- spec/unit/action/puppetfile/purge_spec.rb
+++ spec/unit/action/puppetfile/purge_spec.rb
@@ -2,18 +2,41 @@ require 'spec_helper'
 require 'r10k/action/puppetfile/purge'
 
 describe R10K::Action::Puppetfile::Purge do
+  let(:default_opts) { {root: "/some/nonexistent/path"} }
+  let(:puppetfile) { instance_double('R10K::Puppetfile', :load! => nil) }
 
-  subject { described_class.new({root: "/some/nonexistent/path"}, []) }
+  def purger(opts = {}, argv = [], settings = {})
+    opts = default_opts.merge(opts)
+    return described_class.new(opts, argv, settings)
+  end
 
-  let(:puppetfile) { instance_double('R10K::Puppetfile') }
-
-  before { allow(R10K::Puppetfile).to receive(:new).with("/some/nonexistent/path", nil, nil).and_return(puppetfile) }
+  before(:each) do
+    allow(R10K::Puppetfile).to receive(:new).with("/some/nonexistent/path", nil, nil).and_return(puppetfile)
+  end
 
   it_behaves_like "a puppetfile action"
 
   it "purges unmanaged entries in the Puppetfile moduledir" do
-    allow(puppetfile).to receive(:load!)
     expect(puppetfile).to receive(:purge!)
-    subject.call
+
+    purger.call
+  end
+
+  describe "using custom paths" do
+    before(:each) do
+      allow(puppetfile).to receive(:purge!)
+    end
+
+    it "can use a custom puppetfile path" do
+      expect(R10K::Puppetfile).to receive(:new).with("/some/nonexistent/path", nil, "/some/other/path/Puppetfile").and_return(puppetfile)
+
+      purger({puppetfile: "/some/other/path/Puppetfile"}).call
+    end
+
+    it "can use a custom moduledir path" do
+      expect(R10K::Puppetfile).to receive(:new).with("/some/nonexistent/path", "/some/other/path/site-modules", nil).and_return(puppetfile)
+
+      purger({moduledir: "/some/other/path/site-modules"}).call
+    end
   end
 end
