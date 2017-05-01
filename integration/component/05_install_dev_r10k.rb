b0c48ebbf63813e609d25eeb7c10bed28e404111
libbymo <libbymo@gmail.com>
2017-05-01 14:04:22 -0700

    (CODEMGMT-1064) Test pe-r10k project before promoting it.
    Prior to this commit the acceptance task would only target a previously packaged version of pe-r10k.  This addition allows for the installation of a dev version of pe-r10k over the install of pe. The acceptance task has been modified to allow for this additional component focused test, vs. a pre-suite test targeting a PEZ build. The ci:test:component task will need to be called by a component configuration to be added to ci-job-configs.

---
+++ integration/component/05_install_dev_r10k.rb
@@ -0,0 +1,19 @@
+test_name "Install PE r10k" do
+
+  step "Install PE r10k" do
+    client = find_only_one('client')
+
+    install_pe_product_on(client, 'pe-r10k', ENV['SHA'])
+
+  end
+end
+
+# taken from beaker-pe/lib/beaker-pe/pe-client-tools/install_helper.rb
+def install_pe_product_on(hosts, product, product_sha)
+
+    block_on hosts do |host|
+      variant, version, arch, codename = host['platform'].to_array
+        install_dev_repos_on(product, host, product_sha, '/tmp/repo_configs', {:dev_builds_url => 'http://builds.delivery.puppetlabs.net'})
+        host.install_package(product)
+    end
+  end
