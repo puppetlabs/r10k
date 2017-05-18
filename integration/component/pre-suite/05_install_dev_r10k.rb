a140ccae29333b8d7adacc2ac039e9fb73b52c77
Andrew Roetker <andrew.roetker@puppetlabs.com>
2017-05-18 10:44:03 -0700

    (CODEMGMT-1064) Install pe-r10k on master not undefined host var

--- integration/component/pre-suite/05_install_dev_r10k.rb
+++ integration/component/pre-suite/05_install_dev_r10k.rb
@@ -4,7 +4,7 @@ test_name "Install PE r10k" do
 
     # taken from beaker-pe/lib/beaker-pe/pe-client-tools/install_helper.rb
     install_dev_repos_on('pe-r10k', master, ENV['SHA'], '/tmp/repo_configs', {:dev_builds_url => 'http://builds.delivery.puppetlabs.net'})
-    host.install_package('pe-r10k')
+    master.install_package('pe-r10k')
 
   end
 end
