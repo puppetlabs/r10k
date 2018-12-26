e744c0c3581cdf44d07f8204221f61c575695a38
Ben Ford <ben.ford@puppetlabs.com>
2018-12-26 15:44:33 -0800

    POC hack to parse Puppetfile directly W/O eval
    Ruby 2.6 instroduced the RubyVM::AbstractSyntaxTree, which will parse
    arbitrary Ruby code into an AST. This means that instead of eval'ing
    code, we can just parse it and take action on the three methods we care
    about. In other words, the Puppetfile will no longer be arbitrary and
    unchecked Ruby code.
    
    See https://docs.ruby-lang.org/en/trunk/RubyVM/AbstractSyntaxTree.html
    for more information.
    
    Note: This implementation is *experimental* and will almost certainly
    change. This should not be merged until upstream commits to a stable
    API. But it's worth exploring and validating that we can indeed have
    non-executable Puppetfiles.

--- lib/r10k/puppetfile.rb
+++ lib/r10k/puppetfile.rb
@@ -67,8 +67,19 @@ class Puppetfile
   end
 
   def load!
-    dsl = R10K::Puppetfile::DSL.new(self)
-    dsl.instance_eval(puppetfile_contents, @puppetfile_path)
+    begin
+      logger.debug "Attempting to parse the AST directly"
+
+      parser = R10K::Puppetfile::Parser.new(self)
+      parser.parse(puppetfile_contents)
+
+    rescue R10K::Error => e
+      logger.debug "Falling back to eval'ing the Puppetfile"
+
+      dsl = R10K::Puppetfile::DSL.new(self)
+      dsl.instance_eval(puppetfile_contents, @puppetfile_path)
+    end
+
     validate_no_duplicate_names(@modules)
     @loaded = true
   rescue SyntaxError, LoadError, ArgumentError, NameError => e
@@ -212,6 +223,60 @@ class Puppetfile
     def method_missing(method, *args)
       raise NoMethodError, _("unrecognized declaration '%{method}'") % {method: method}
     end
+  end
+
+  class Parser
+    # A POC Puppetfile parser that does not require eval'ing Ruby code
+    #
+    # @api private
+
+    def initialize(librarian)
+      @librarian = librarian
+    end
+
+    def parse(puppetfile)
+      root = nil
+      begin
+        root = RubyVM::AbstractSyntaxTree.parse(puppetfile)
+      rescue NameError => e
+        # When run on Ruby 2.6 or greater, this will parse the Puppetfile directly.
+        # See https://docs.ruby-lang.org/en/trunk/RubyVM/AbstractSyntaxTree.html for more infos
+        raise R10K::Error.new("Cannot parse Puppetfile directly on Ruby version #{RUBY_VERSION}")
+      end
+      traverse(root)
+    end
+
+    def traverse(node)
+      begin
+        if node.type == :FCALL
+          name = node.children.first
+          args = node.children.last.children.map do |item|
+            next if item.nil?
+            item.children.first
+          end.compact
+
+          case name
+          when :mod
+            @librarian.add_module(args.shift, *args)
+          when :forge
+            @librarian.set_forge(args.shift)
+          when :moduledir
+            @librarian.set_moduledir(args.shift)
+          else
+            # Should we log unexpected Ruby code?
+          end
+        end
+
+        node.children.each do |n|
+          next unless n.is_a? RubyVM::AbstractSyntaxTree::Node
+
+          traverse(n)
+        end
+      rescue => e
+        puts e.message
+      end
+    end
+
   end
 end
 end
