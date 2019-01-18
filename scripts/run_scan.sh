608b1f105b894a756f552f7198b7eed7b95d56bc
Jayant Sane <ghjsane@gmail.com>
2019-01-18 14:37:46 -0800

    TEST - DO NOT MERGE: Experimental changes to enable checkmarkx scans in Travis-26

--- scripts/run_scan.sh
+++ scripts/run_scan.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ev
 
+bundle exec rspec --color --format documentation spec/unit
+
 if [ ${CHECK} == CXSCAN ]; then
   $USERDIR/CxConsolePlugin-8.80.0/runCxConsole.sh
 elif [ ${CHECK} == DUMMY ]; then
