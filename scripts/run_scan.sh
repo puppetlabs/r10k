c7e40302acb94bb8452a545af346d28ce676ff8c
Jayant Sane <ghjsane@gmail.com>
2019-01-24 10:36:03 -0800

    TEST - DO NOT MERGE: Experimental changes to enable checkmarkx scans - real attempt 2

--- scripts/run_scan.sh
+++ scripts/run_scan.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -ev
 
+echo `pwd`
 
 if [ ${CHECK} == CXSCAN ]; then
-  $USERDIR/CxConsolePlugin-8.80.0/runCxConsole.sh
+  $USERDIR/CxConsolePlugin-8.80.0/runCxConsole.sh -v -Projectname "CxServer\r10k" -CxServer http://10.234.4.100 -cxuser admin -cxpassword ChkDemo123* -preset "Checkmarx Default" -LocationType folder -locationpath /home/travis/build/puppetlabs/r10k/
 elif [ ${CHECK} == DUMMY ]; then
   echo "Dummy invocation - scan skipped"
 fi
