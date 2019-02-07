4522800b05e9b1374c6273c02ff9f7d004e489ae
Morgan Rhodes <morgan@puppet.com>
2019-02-07 14:38:20 -0800

    (maint) Enable analytics for r10k container

--- docker/Makefile
+++ docker/Makefile
@@ -1,3 +1,4 @@
+PUPPERWARE_ANALYTICS_STREAM ?= dev
 git_describe = $(shell git describe)
 vcs_ref := $(shell git rev-parse HEAD)
 build_date := $(shell date -u +%FT%T)
@@ -29,7 +30,15 @@ else
 endif
 
 build: prep
-	@docker build --pull --build-arg vcs_ref=$(vcs_ref) --build-arg build_date=$(build_date) --build-arg version=$(version) --file r10k/$(dockerfile) --tag puppet/r10k:$(version) r10k
+	@docker build \
+		--pull \
+		--build-arg vcs_ref=$(vcs_ref) \
+		--build-arg build_date=$(build_date) \
+		--build-arg version=$(version) \
+		--build-arg pupperware_analytics_stream=$(PUPPERWARE_ANALYTICS_STREAM) \
+		--file r10k/$(dockerfile) \
+		--tag puppet/r10k:$(version) \
+		r10k
 ifeq ($(IS_LATEST),true)
 	@docker tag puppet/r10k:$(version) puppet/r10k:latest
 endif
---
+++ docker/r10k/docker-entrypoint.d/10-analytics.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ "${PUPPERWARE_DISABLE_ANALYTICS}" = "true" ]; then
+    echo "($0) Pupperware analytics disabled; skipping metric submission"
+    exit 0
+fi
+
+# See: https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
+# Tracking ID
+tid=$PUPPERWARE_ANALYTICS_TRACKING_ID
+# Application Name
+an=r10k
+# Application Version
+av=$R10K_VERSION
+# Anonymous Client ID
+_file=/var/tmp/pwclientid
+cid=$(cat $_file 2>/dev/null || (cat /proc/sys/kernel/random/uuid | tee $_file))
+# Event Category
+ec=${PUPPERWARE_ANALYTICS_STREAM:-dev}
+# Event Action
+ea=start
+
+_params="v=1&t=event&tid=${tid}&an=${an}&av=${av}&cid=${cid}&ec=${ec}&ea=${ea}"
+_url="http://www.google-analytics.com/collect?${_params}"
+
+echo "($0) Sending metrics ${_url}"
+curl --fail --silent --show-error --output /dev/null \
+    -X POST -H "Content-Length: 0" $_url
