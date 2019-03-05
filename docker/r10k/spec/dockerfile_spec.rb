da796d20f577ab2586fb3ddb218728d06c841d1a
Iristyle <Iristyle@github>
2019-03-05 14:12:02 -0800

    (maint) Refactor Docker test CLI invocations
     - Move from using %x to popen2e
    
       %x seems to be causing problems with Azure runs, causing failures
       with no discernible output.
    
       For commands with no captured output, this streams stdout / stderr
       as commands execute, and returns the Process::Status return value
       upon completion (which has an exitstatus value)

--- docker/r10k/spec/dockerfile_spec.rb
+++ docker/r10k/spec/dockerfile_spec.rb
@@ -1,9 +1,26 @@
 require 'rspec/core'
 require 'fileutils'
+require 'open3'
 
 SPEC_DIRECTORY = File.dirname(__FILE__)
 
 describe 'r10k container' do
+
+  def run_command(command)
+    status = nil
+    STDOUT.puts "Executing #{command}"
+    Open3.popen2e(command) do |stdin, stdout_stderr, wait_thread|
+      Thread.new do
+        stdout_stderr.each { |l| STDOUT.puts l }
+      end
+
+      stdin.close
+      status = wait_thread.value
+    end
+
+    status
+  end
+
   before(:all) do
     @image = ENV['PUPPET_TEST_DOCKER_IMAGE']
     if @image.nil?
@@ -22,24 +39,24 @@ describe 'r10k container' do
                --volume #{File.join(SPEC_DIRECTORY, 'fixtures')}:/test \
                #{@image}).chomp
 
-    %x(docker exec #{@container} cp test/Puppetfile /)
+    run_command("docker exec #{@container} cp test/Puppetfile /")
   end
 
   after(:all) do
-    %x(docker container kill #{@container})
+    run_command("docker container kill #{@container}")
     FileUtils.rm_rf(File.join(SPEC_DIRECTORY, 'fixtures', 'modules'))
   end
 
   it 'should validate the Puppetfile' do
-    %x(docker exec #{@container} r10k puppetfile check)
-    status = $?
-    expect(status).to eq(0)
+    cmd = "docker exec #{@container} r10k puppetfile check"
+    status = run_command(cmd)
+    expect(status.exitstatus).to eq(0)
   end
 
   it 'should install the Puppetfile' do
-    %x(docker exec #{@container} r10k puppetfile install)
-    status = $?
-    expect(status).to eq(0)
+    cmd = "docker exec #{@container} r10k puppetfile install"
+    status = run_command(cmd)
+    expect(status.exitstatus).to eq(0)
     expect(Dir.exist?(File.join(SPEC_DIRECTORY, 'fixtures', 'modules', 'ntp'))).to eq(true)
   end
 end
