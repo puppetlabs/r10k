5f544db3370b288be18ba219d7751ed4fc900989
Romain Tartière <romain@blogreen.org>
2019-07-26 14:15:24 -1000

    (maint) Consistently compare values to nil
    `overrides[:deploy]` is initialized as an empty Hash if any of
    puppet-path and generate-types is set.
    
    `overrides[:deploy][:puppet_path]` and
    `overrides[:deploy][:generate_types]` are respectively set when
    puppet-path and generate-types are non-nil.
    
    `false` being not nil, when generate-types is set to `false`,
    `overrides[:deploy]` will not be initialized and an excpetion will be
    raised when setting `overrides[:deploy][:generate_types]` to false.
    
    Always compare values to nil to avoid such a situation.

--- lib/r10k/action/runner.rb
+++ lib/r10k/action/runner.rb
@@ -44,7 +44,7 @@ module R10K
 
         overrides = {}
         overrides[:cachedir] = @opts[:cachedir] unless @opts[:cachedir].nil?
-        overrides[:deploy] = {} if @opts[:'puppet-path'] || @opts[:'generate-types']
+        overrides[:deploy] = {} if !@opts[:'puppet-path'].nil? || !@opts[:'generate-types'].nil?
         overrides[:deploy][:puppet_path] = @opts[:'puppet-path'] unless @opts[:'puppet-path'].nil?
         overrides[:deploy][:generate_types] = @opts[:'generate-types'] unless @opts[:'generate-types'].nil?
 
