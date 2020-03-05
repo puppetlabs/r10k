df81640871ca6972b5e662d3eb16f6a58610b7da
Molly Waggett <molly.waggett@puppet.com>
2020-03-05 09:53:16 -0800

    (OSOKR-23) Add GitHub Action to tag and release
    This commit adds a GitHub Action to tag the r10k repo, build the r10k gem, and
    publish the gem to rubygems. This Action will be triggered whenever r10k's
    version file gets updated. The commit message to update the version file must
    include `#minor` or `#major` in order to bump the Y or X version. Otherwise,
    the Z version will get bumped. For more info, see
    https://github.com/marketplace/actions/github-tag-bump

--- lib/r10k/version.rb
+++ lib/r10k/version.rb
@@ -1,3 +1,6 @@
 module R10K
+  # When updating to a new major (X) or minor (Y) version, include `#major` or
+  # `#minor` (respectively) in your commit message to trigger the appropriate
+  # release. Otherwise, a new patch (Z) version will be released.
   VERSION = '2.6.7'
 end
