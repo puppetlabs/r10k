804bd5752dc5cbf45c76144bb71113b1cef38cf3
Molly Waggett <molly.waggett@puppet.com>
2020-03-06 10:21:29 -0800

    (maint) Prep for 2.6.8 release

--- lib/r10k/version.rb
+++ lib/r10k/version.rb
@@ -2,5 +2,5 @@ module R10K
   # When updating to a new major (X) or minor (Y) version, include `#major` or
   # `#minor` (respectively) in your commit message to trigger the appropriate
   # release. Otherwise, a new patch (Z) version will be released.
-  VERSION = '2.6.7'
+  VERSION = '2.6.8'
 end
