a357df97261af9d6111cee5a44aa82978feb7194
Molly Waggett <molly.waggett@puppet.com>
2020-06-08 10:51:21 -0700

    (maint) Prep for 3.5.1 release

--- lib/r10k/version.rb
+++ lib/r10k/version.rb
@@ -2,5 +2,5 @@ module R10K
   # When updating to a new major (X) or minor (Y) version, include `#major` or
   # `#minor` (respectively) in your commit message to trigger the appropriate
   # release. Otherwise, a new patch (Z) version will be released.
-  VERSION = '3.5.0'
+  VERSION = '3.5.1'
 end
