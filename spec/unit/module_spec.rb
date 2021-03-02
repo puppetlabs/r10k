afe6033b432794503a0d126ab7b66e2c637ceb55
Reid Vandewiele <reid@puppet.com>
2021-03-02 15:30:23 -0800

    Implement standard module option schema
    Allow modules of any currently supported type to be initialized using
    the following standard specification:
    
      ---
      <module-name>:
        type: <type>
        source: <source>
        version: <version>
    
    This is in slight contrast to today's expectations, which are largely
    based on the presence of a type-specific key to indicate the kind of
    module to create, and there are no standard parameters to indicate
    source and version.
    
    By providing a standard schema for defining modules of any type, this
    paves the way for easier integration with external data systems which
    may define sets of modules of many types to include in different
    environments.
    
    This schema is generalized away from implementation and towards the
    concepts that matter to the user.

--- spec/unit/module_spec.rb
+++ spec/unit/module_spec.rb
@@ -3,20 +3,45 @@ require 'r10k/module'
 
 describe R10K::Module do
   describe 'delegating to R10K::Module::Git' do
-    it "accepts args {:git => 'git url}" do
-      obj = R10K::Module.new('foo', '/modulepath', :git => 'git url')
-      expect(obj).to be_a_kind_of(R10K::Module::Git)
+    [ {git: 'git url'},
+      {type: 'git', source: 'git url'},
+    ].each do |scenario|
+      it "accepts a name matching 'test' and args #{scenario.inspect}" do
+        obj = R10K::Module.new('test', '/modulepath', scenario)
+        expect(obj).to be_a_kind_of(R10K::Module::Git)
+        expect(obj.send(:instance_variable_get, :'@remote')).to eq('git url')
+      end
+    end
+  end
+
+  describe 'delegating to R10K::Module::Svn' do
+    [ {svn: 'svn url'},
+      {type: 'svn', source: 'svn url'},
+    ].each do |scenario|
+      it "accepts a name matching 'test' and args #{scenario.inspect}" do
+        obj = R10K::Module.new('test', '/modulepath', scenario)
+        expect(obj).to be_a_kind_of(R10K::Module::SVN)
+        expect(obj.send(:instance_variable_get, :'@url')).to eq('svn url')
+      end
     end
   end
 
   describe 'delegating to R10K::Module::Forge' do
-    [
-      ['bar/quux', nil],
-      ['bar-quux', nil],
-      ['bar/quux', '8.0.0'],
+    [ 'bar/quux',
+      'bar-quux',
     ].each do |scenario|
-      it "accepts a name matching #{scenario[0]} and args #{scenario[1].inspect}" do
-        expect(R10K::Module.new(scenario[0], '/modulepath', scenario[1])).to be_a_kind_of(R10K::Module::Forge)
+      it "accepts a name matching #{scenario} and args nil" do
+        obj = R10K::Module.new(scenario, '/modulepath', nil)
+        expect(obj).to be_a_kind_of(R10K::Module::Forge)
+      end
+    end
+    [ '8.0.0',
+      {type: 'forge', version: '8.0.0'},
+    ].each do |scenario|
+      it "accepts a name matching bar-quux and args #{scenario.inspect}" do
+        obj = R10K::Module.new('bar-quux', '/modulepath', scenario)
+        expect(obj).to be_a_kind_of(R10K::Module::Forge)
+        expect(obj.send(:instance_variable_get, :'@expected_version')).to eq('8.0.0')
       end
     end
   end
